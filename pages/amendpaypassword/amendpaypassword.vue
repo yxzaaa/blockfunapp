7c4f90665b8190b88bb6a04b4fdd7fd9933e64ed
yxzaaa <2280517552@qq.com>
2019-09-25 17:45:07 +0800

    1

--- pages/amendpaypassword/amendpaypassword.vue
+++ pages/amendpaypassword/amendpaypassword.vue
@@ -7,14 +7,20 @@
 		/>
 		<view class="app-container full" style="padding-left:40upx;padding-right:40upx;">
 			<view style="width:670upx;line-height:68upx;color:#fff;font-size: 48upx;">
-				修改支付密码
+				{{stepLib[currStep].title}}
 			</view>
-			<view style="line-height:40upx;color:#fff;font-size: 28upx;padding-top:30upx;">请输入原支付密码</view>
-			<view style="width:670upx;padding-top:60upx;">
-				<possword-inputer size="74upx" borderColor="rgba(255,255,255,0.4)" fontSize="48upx"></possword-inputer>
+			<view style="line-height:40upx;color:#fff;font-size: 28upx;padding-top:30upx;padding-bottom:60upx;">{{stepLib[currStep].content}}</view>
+			<view style="width:670upx;">
+				<possword-inputer @input="setPassword" v-if="currStep == 0" size="74upx" borderColor="rgba(255,255,255,0.4)" fontSize="48upx"></possword-inputer>
+			</view>
+			<view style="width:670upx;">
+				<possword-inputer @input="setPassword" v-if="currStep == 1" size="74upx" borderColor="rgba(255,255,255,0.4)" fontSize="48upx"></possword-inputer>
+			</view>
+			<view style="width:670upx;">
+				<possword-inputer @input="setPassword" v-if="currStep == 2" size="74upx" borderColor="rgba(255,255,255,0.4)" fontSize="48upx"></possword-inputer>
 			</view>
 			<view style="color:#DA53A2;font-size: 28upx;padding-top:30upx;padding-bottom:80upx;">忘记密码？</view>
-			<fun-button value="提交" block large width="670upx"></fun-button>
+			<fun-button value="确认" block large width="670upx" @handle="confirmStep"></fun-button>
 		</view>
 	</view>
 </template>
@@ -39,14 +45,87 @@
 						type:'normal',
 						text:'取消'
 					},		
-				}
+				},
+				stepLib:[
+					{
+						title:'修改支付密码',
+						content:'请输入原支付密码'
+					},
+					{
+						title:'输入新支付密码',
+						content:'请输入新支付密码'
+					},
+					{
+						title:'确认新支付密码',
+						content:'请再次输入新支付密码'
+					},
+				],
+				currStep:0,
+				oldPassword:'',
+				newPassWord:'',
+				confirmPassword:''
 			}
 		},
 		onPageScroll(val){
 			this.scroll = val.scrollTop;
 		},
 		methods: {
-			
+			setPassword(val){
+				if(this.currStep == 0){
+					this.oldPassword = val;
+				}else if(this.currStep == 1){
+					this.newPassWord = val;
+				}else if(this.currStep == 2){
+					this.confirmPassword = val;
+				}
+			},
+			confirmStep(){
+				if(this.currStep == 0){
+					//验证原支付密码
+					if(this.oldPassword.length == 8){
+						this.currStep = 1;
+					}
+				}else if(this.currStep == 1){
+					this.currStep = 2;
+				}else if(this.currStep == 2){
+					//修改支付密码
+					if(this.newPassWord == this.confirmPassword){
+						this.$http({
+							url:'/v1/main/users/reset-confirm-account-payment-password-by-password',
+							data:{
+								password:this.oldPassword,
+								pay_password:this.newPassWord,
+								pay_password_hash:this.$md5(this.newPassWord)
+							},
+							success:res=>{
+								if(res.code == 200){
+									uni.showToast({
+										title:'密码修改成功',
+										icon:'none'
+									})
+									setTimeout(()=>{
+										uni.navigateBack({
+											delta:1
+										})
+									},1500)
+								}else{
+									uni.showToast({
+										title:res.message,
+										icon:'none'
+									})
+									this.currStep = 0;
+								}
+							}
+						})
+					}else{
+						uni.showToast({
+							title:'两次输入的密码不一致',
+							icon:'none'
+						});
+						this.currStep = 0;
+					}
+				}
+			}
 		}
 	}
 </script>
